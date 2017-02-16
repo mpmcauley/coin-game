a789024280a9d4ad0982c638cf4df6406f1af5c1
Mike McAuley <mmcaule1@lion.lmu.edu>
2017-02-16 15:24:09 -0800

    defined callback in index

--- index.js
+++ index.js
@@ -24,22 +24,31 @@ io.on('connection', (socket) => {
   // messages until a name is accepted. When a name is finally accepted, send a `welcome`
   // message and a the current game state, "turn off" the `name` message listener, then
   // start accepting `move` messages.
+
   const nameListener = (name) => {
     const trimmedName = name.trim();
-    if (game.addPlayer(trimmedName)) {
-      io.to(socket.id).emit('welcome');
-      io.emit('state', game.state());
-      socket.removeListener('name', nameListener);
-      socket.on('move', (direction) => {
-        game.move(direction, trimmedName);
-        io.emit('state', game.state());
-      });
-    } else {
-      io.to(socket.id).emit('badname', trimmedName);
-    }
-  };
+    game.addPlayer(trimmedName, (err, nameWorks) => {
+      if (nameWorks) {
+        io.to(socket.id).emit('welcome');
+        game.state((err, res) => {
+          io.emit('state', res);
+        });
+        socket.removeListener('name', nameListener);
+        socket.on('move', (direction) => {
+          game.move(direction, trimmedName, (err) => {
+            game.state((err,res2) => {
+              io.emit('state', res2);
+            });
+          });
+
+        });
+      } else {
+        io.to(socket.id).emit('badname', trimmedName);
+      }
+
+    ))};
   socket.on('name', nameListener);
-});
+};
 
 // It begins (https://xkcd.com/1656/)
 const port = process.env.PORT || 3000;
